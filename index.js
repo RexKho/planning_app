b79c87b224b413de5cecd69502b5743973db6338
Rex Kho <rexkho510@gmail.com>
2023-08-08 13:08:59 -0700

    Create functional firstwordsaidonline API

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get('/firstwordsaidonline', (_, res)=> res.json('lo'));
+
+const callback = () => console.log('Planning App is running on post 5001!')
+
+app.listen(5001, callback)
